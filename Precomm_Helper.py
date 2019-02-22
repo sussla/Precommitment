e8a57ac8daa1f98ff7b726aa7b3a0909ea14d7f4
Lauren Desktop <laurendesktop@dhcp-acadmin-128-197-62-227.bu.edu>
2019-02-22 17:39:15 -0500

    2.22

--- Precomm_Helper.py
+++ Precomm_Helper.py
@@ -17,10 +17,10 @@ def pickvalues():
     # derived params
     b1 = a1 + w 
     b2 = a2 + w 
-    #pick options 
+    # pick options
     optionA = random.choice(range(a1, b1, 1))
     optionB = random.choice(range(a2, b2, 1))
-    #end values 
+    # end values
     endA = random.choice(range(a1, b1, 1))
     endB = random.choice(range(a2, b2, 1))
     optionValues = {'optionA':optionA, 'optionB':optionB, 'endA':endA, 'endB':endB}
@@ -31,22 +31,22 @@ def pickoptions(win, length):
     pickoptionsClock = core.Clock()
     response = 0
     RT_choice = 0
-    choice_1 = 0 #choice_1 refers to the pick or play routine
+    choice_1 = 0 # choice_1 refers to the pick or play routine
     ### start routine "pick options ###
     core.wait(1)
     win.flip()
-    #record responses
+    # record responses
     theseKeys = event.getKeys(keyList=['1', '2', 'escape'])
     if len(theseKeys) > 0:
-        if '1' in theseKeys: #pick play
+        if '1' in theseKeys: # pick play
             response = 1
             RT_choice = pickoptionsClock.getTime()
             win.flip()
-        elif '2' in theseKeys: #pick pick
+        elif '2' in theseKeys: # pick pick
             response = 2
             RT_choice = pickoptionsClock.getTime()
             win.flip()
-        else: #did not pick
+        else: # did not pick
             response = 3
             RT_choice = pickoptionsClock.getTime()
             win.flip()
@@ -80,7 +80,7 @@ def precomm_pick(win, length):
     if response == 0:
         response = 3
         RT_precomm = pickClock.getTime()
-    choice_2 ={'right':response == 1, 'left':response == 2, 'miss':response == 3, 'RT_precomm': RT_precomm}
+    choice_2 = {'right':response == 1, 'left':response == 2, 'miss':response == 3, 'RT_precomm': RT_precomm}
     return choice_2 
 
 #### play routine (MID routine) ####
@@ -88,48 +88,80 @@ def play_routine(win, length):
     playClock = core.Clock()
     playClock.reset()
     response = 0
+    played = 0
     late_reaction = 0
-    length = 0.5
+    #length = 0.5
     RT_play = 0
     choice_3 = 0 #choice_3 refers to the play routine 
     ### start routine "play" ###
     theseKeys = event.getKeys(keyList=['1', '2', 'escape'])
-    while playClock.getTime() < length:
-        if len(theseKeys) > 0:
-            if '1' in theseKeys: #pick right 
+    if len(theseKeys) > 0:
+        if '1' in theseKeys: #pick right
+            if playClock.getTime() < 0.5:
                 response = 1
+                print('a')
+                played = 1
                 RT_play = playClock.getTime()
                 win.flip()
-            elif '2' in theseKeys: # pick left 
-                response = 2
-                RT_play = playClock.getTime()
-                win.flip()
-            else: #did not pick
+            elif playClock.getTime() >= 0.5:
                 response = 3
-                RT_play = playClock.getTime()
-                win.flip()
-    while playClock.getTime() > length:
-        if len(theseKeys) > 0:
-            if '1' in theseKeys: #picked right too late
-                response = 3
+                print('one')
                 late_reaction = 4
                 RT_play = playClock.getTime()
                 win.flip()
-            elif '2' in theseKeys: #picked left too late
+        elif '2' in theseKeys:
+            if playClock.getTime() < 0.5: # pick left
+                response = 2
+                print('b')
+                played = 1
+                RT_play = playClock.getTime()
+                win.flip()
+            elif playClock.getTime() >= 0.5:
                 response = 3
+                print('two')
                 late_reaction = 5
                 RT_play = playClock.getTime()
                 win.flip()
-            else: #still did not pick
-                response = 3
-                late_reaction = 6
-                RT_play = playClock.getTime()
-                win.flip()
-    if response == 0: 
+        else: #did not pick
+            response = 3
+            print('three')
+            late_reaction = 6
+            win.flip()
+    if response == 0:
         response = 3
-        RT_play = playClock.getTime()
+        win.flip()
+        print('whoops')
     choice_3 = {'win_right':response == 1, 'win_left':response == 2, 'loose':response == 3,
-                'late_right': late_reaction == 4, 'late_left': late_reaction ==5,
+                'late_right': late_reaction == 4, 'late_left': late_reaction == 5,
                 'never_press': late_reaction == 6,
                 'RT_play': RT_play}
     return choice_3
+
+def press_late(win, length ):
+    lateClock = core.Clock()
+    response = 0
+    RT_late = 0
+    choice_4 = 0 #choice_2 refers to the precommitment routine
+    #record responses
+    theseKeys = event.getKeys(keyList=['1', '2', 'escape'])
+    if len(theseKeys) > 0:
+        if '1' in theseKeys: #pick right
+            response = 1
+            print('hello')
+            RT_late = lateClock.getTime()
+            win.flip()
+        elif '2' in theseKeys: #pick left
+            print('world')
+            response = 2
+            RT_late = lateClock.getTime()
+            win.flip()
+        else: #did not pick
+            print('yup')
+            response = 3
+            RT_late = lateClock.getTime()
+            win.flip()
+    if response == 0:
+        response = 3
+        RT_late = lateClock.getTime()
+    choice_2 = {'late_right':response == 1, 'late_left':response == 2, 'not_late':response == 3, 'RT_late': RT_late}
+    return choice_2
